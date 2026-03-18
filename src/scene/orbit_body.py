a2b5c2c17b1f7d0d20dbf4f2ddb8e835d51baaab
csvitor-dev <csvitor.dev@gmail.com>
2026-03-18 12:27:47 -0300

    feat: add linear transformations

---
+++ src/scene/orbit_body.py
@@ -0,0 +1,36 @@
+import pyrr
+
+
+class OrbitBody:
+
+    def __init__(self, distance, size, speed):
+
+        self.distance = distance
+        self.size = size
+        self.speed = speed
+
+        self.angle = 0
+
+    def update(self, dt):
+        self.angle += self.speed * dt
+
+    def matrix(self):
+
+        rotation = pyrr.matrix44.create_from_z_rotation(self.angle)
+
+        translation = pyrr.matrix44.create_from_translation(
+            [self.distance, 0, 0]
+        )
+
+        scale = pyrr.matrix44.create_from_scale([self.size, self.size, 1])
+
+        m = pyrr.matrix44.multiply(scale, translation)
+        m = pyrr.matrix44.multiply(m, rotation)
+
+        projection = pyrr.matrix44.create_orthogonal_projection(
+            -1, 1,
+            -1, 1,
+            -1, 1
+        )
+
+        return m, projection
---
+++ src/scene/transform.py
@@ -0,0 +1,17 @@
+import pyrr
+
+
+class Transform:
+
+    def __init__(self):
+        self.translation = pyrr.Vector3([0, 0, 0])
+        self.rotation = 0
+        self.scale = pyrr.Vector3([1, 1, 1])
+
+    def matrix(self):
+
+        T = pyrr.matrix44.create_from_translation(self.translation)
+        R = pyrr.matrix44.create_from_z_rotation(self.rotation)
+        S = pyrr.matrix44.create_from_scale(self.scale)
+
+        return pyrr.matrix44.multiply(S, pyrr.matrix44.multiply(T, R))
